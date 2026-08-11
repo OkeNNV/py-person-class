8bd9ad52ae8fffebb58364f2cc9365bdf4c28353
Artem D <OkeNNV@gmail.com>
2026-08-11 12:38:10 +0300

    Person class implementation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    def set_spouse(
+            self, spouse_name: str, is_wife: bool = True
+    ) -> None:
+        spouse = Person.people[spouse_name]
+
+        if is_wife:
+            # setattr to bypass static type linter warnings
+            setattr(self, "wife", spouse)
+            spouse.husband = self
+        else:
+            setattr(self, "husband", spouse)
+            spouse.wife = self
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+
+        new_person = Person(name=person["name"], age=person["age"])
+
+        if person.get("wife") in Person.people.keys():
+            new_person.set_spouse(
+                spouse_name = person["wife"],
+                is_wife=True
+            )
+
+        if person.get("husband") in Person.people.keys():
+            new_person.set_spouse(
+                spouse_name = person["husband"],
+                is_wife=False
+            )
+
+    return list(Person.people.values())
